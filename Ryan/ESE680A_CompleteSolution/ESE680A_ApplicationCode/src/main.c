d7fbcdc4016d41f1151bef8bed0bbd168741a8dd
rspice <rspicer@gmail.com>
2017-03-23 15:49:53 -0400

    Flailing
    Main app spams serial and lights an LED at least

--- Ryan/ESE680A_CompleteSolution/ESE680A_ApplicationCode/src/main.c
+++ Ryan/ESE680A_CompleteSolution/ESE680A_ApplicationCode/src/main.c
@@ -29,10 +29,60 @@
  */
 #include <asf.h>
 
+struct usart_module usart_instance;
+#define MAX_RX_BUFFER_LENGTH   5
+#define EDBG_CDC_MODULE              SERCOM4
+#define EDBG_CDC_SERCOM_MUX_SETTING  USART_RX_3_TX_2_XCK_3
+#define EDBG_CDC_SERCOM_PINMUX_PAD0  PINMUX_UNUSED
+#define EDBG_CDC_SERCOM_PINMUX_PAD1  PINMUX_UNUSED
+#define EDBG_CDC_SERCOM_PINMUX_PAD2  PINMUX_PB10D_SERCOM4_PAD2
+#define EDBG_CDC_SERCOM_PINMUX_PAD3  PINMUX_PB11D_SERCOM4_PAD3
+
+#define LED0_PIN                  PIN_PA23
+#define LED0_ACTIVE               false
+#define LED0_INACTIVE             !LED0_ACTIVE
+
+/** UART module for debug. */
+static struct usart_module cdc_uart_module;
+
+/**
+ * \brief Configure UART console.
+ */
+static void configure_console(void)
+{
+	struct usart_config usart_conf;
+
+	usart_get_config_defaults(&usart_conf);
+	usart_conf.mux_setting = EDBG_CDC_SERCOM_MUX_SETTING;
+	usart_conf.pinmux_pad0 = EDBG_CDC_SERCOM_PINMUX_PAD0;
+	usart_conf.pinmux_pad1 = EDBG_CDC_SERCOM_PINMUX_PAD1;
+	usart_conf.pinmux_pad2 = EDBG_CDC_SERCOM_PINMUX_PAD2;
+	usart_conf.pinmux_pad3 = EDBG_CDC_SERCOM_PINMUX_PAD3;
+	usart_conf.baudrate    = 115200;
+
+	stdio_serial_init(&cdc_uart_module, EDBG_CDC_MODULE, &usart_conf);
+	usart_enable(&cdc_uart_module);
+}
+
 int main (void)
 {
 	system_init();
 	system_interrupt_enable_global();
+	struct port_config pin_conf;
+	port_get_config_defaults(&pin_conf);
+
+	/* Configure LEDs as outputs, turn them off */
+	pin_conf.direction  = PORT_PIN_DIR_OUTPUT;
+	port_pin_set_config(LED0_PIN, &pin_conf);
+	port_pin_set_output_level(LED0_PIN, true);
+	configure_console();
+	while(1) {
+		port_pin_toggle_output_level(LED0_PIN);
+		printf("I'M ALIVE\n");
+		//delay_s(1);
+		//delay_cycles(1000);
+		//delay_ms(100);
+	}
 
 	/* Insert application code here, after the board has been initialized. */
 }
