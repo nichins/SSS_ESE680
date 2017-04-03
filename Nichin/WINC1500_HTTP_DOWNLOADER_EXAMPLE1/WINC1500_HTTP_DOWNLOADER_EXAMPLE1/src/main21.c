2cf18a22ca37d2bbae568f6a5087037d2b2359a3
Nichin Sreekantaswamy <nichins@seas.upenn.edu>
2017-04-02 23:20:04 -0400

    Added HTTP file download and write to flash

---
+++ Nichin/WINC1500_HTTP_DOWNLOADER_EXAMPLE1/WINC1500_HTTP_DOWNLOADER_EXAMPLE1/src/main21.c
@@ -0,0 +1,578 @@
+/**
+ *
+ * \file
+ *
+ * \brief HTTP File Downloader Example.
+ *
+ * Copyright (c) 2016 Atmel Corporation. All rights reserved.
+ *
+ * \asf_license_start
+ *
+ * \page License
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. The name of Atmel may not be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ATMEL "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT ARE
+ * EXPRESSLY AND SPECIFICALLY DISCLAIMED. IN NO EVENT SHALL ATMEL BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * \asf_license_stop
+ *
+ */
+
+/** \mainpage
+ * \section intro Introduction
+ * This example demonstrates how to connect to an HTTP server and download
+ * a file using HTTP client.<br>
+ * It uses the following hardware:
+ * - the SAM Xplained Pro.
+ * - the WINC1500 on EXT1.
+ * - the IO1 Xplained Pro on EXT2.
+ *
+ * \section files Main Files
+ * - main.c : Initialize the SD card and WINC1500 Wi-Fi module, then download 
+ * a file using HTTP client.
+ *
+ * \section usage Usage
+ * -# Configure below code in the main.h for AP information to be connected.
+ * \code
+ *    #define MAIN_WLAN_SSID         "DEMO_AP"
+ *    #define MAIN_WLAN_AUTH         M2M_WIFI_SEC_WPA_PSK
+ *    #define MAIN_WLAN_PSK          "12345678"
+ * \endcode
+ *
+ * -# Configure HTTP URL macro in the main.h file.
+ * \code
+ *    #define MAIN_HTTP_FILE_URL                   "http://www.atmel.com/Images/45093A-SmartConnectWINC1500_E_US_101014_web.pdf"
+ * \endcode
+ *
+ * -# Build the program and download it into the board.
+ * -# On the computer, open and configure a terminal application as following.
+ * \code
+ *    Baud Rate : 115200
+ *    Data : 8bit
+ *    Parity bit : none
+ *    Stop bit : 1bit
+ *    Flow control : none
+ * \endcode
+ *
+ * -# Start the application.
+ * -# In the terminal window, the following text should appear:<br>
+ *
+ * \code
+ *    -- HTTP file downloader example --
+ *    -- SAMXXX_XPLAINED_PRO --
+ *    -- Compiled: xxx xx xxxx xx:xx:xx --
+ *
+ *    This example requires the AP to have internet access.
+ *
+ *    init_storage: please plug an SD/MMC card in slot...
+ *    init_storage: mounting SD card...
+ *    init_storage: SD card mount OK.
+ *    main: connecting to WiFi AP DEMO_AP...
+ *    wifi_cb: M2M_WIFI_CONNECTED
+ *    wifi_cb: IP address is 192.168.1.107
+ *    start_download: sending HTTP request...
+ *    resolve_cb: www.atmel.com IP address is 72.246.56.186
+ *
+ *    http_client_callback: HTTP client socket connected.
+ *    http_client_callback: request completed.
+ *    http_client_callback: received response 200 data size 1147097
+ *    store_file_packet: creating file [0:45093A-SmartConnectWINC1500_E_US_101014_web.pdf]
+ *    store_file_packet: received[xxx], file size[1147097]
+ *    ...
+ *    store_file_packet: received[1147097], file size[1147097]
+ *    store_file_packet: file downloaded successfully.
+ *    main: please unplug the SD/MMC card.
+ *    main: done.
+ * \endcode
+ *
+ * \section compinfo Compilation Information
+ * This software was written for the GNU GCC compiler using Atmel Studio 6.2
+ * Other compilers may or may not work.
+ *
+ * \section contactinfo Contact Information
+ * For further information, visit
+ * <A href="http://www.atmel.com">Atmel</A>.\n
+ */
+
+#include <errno.h>
+#include "asf.h"
+#include "main.h"
+#include "stdio_serial.h"
+#include "driver/include/m2m_wifi.h"
+#include "socket/include/socket.h"
+#include "iot/http/http_client.h"
+
+#define STRING_EOL                      "\r\n"
+#define STRING_HEADER                   "-- HTTP file downloader example --"STRING_EOL \
+	"-- "BOARD_NAME " --"STRING_EOL	\
+	"-- Compiled: "__DATE__ " "__TIME__ " --"STRING_EOL
+
+#define AT25DFX_BUFFER_SIZE  (10)
+#define AT25DFX_CLOCK_SPEED				120000
+#define AT25DFX_SPI_PINMUX_SETTING		SPI_SIGNAL_MUX_SETTING_E
+#define AT25DFX_SPI_PINMUX_PAD0			PINMUX_PA16C_SERCOM1_PAD0
+#define AT25DFX_SPI_PINMUX_PAD1			PINMUX_PA17C_SERCOM1_PAD1
+#define AT25DFX_SPI_PINMUX_PAD2			PINMUX_PA18C_SERCOM1_PAD2
+#define AT25DFX_SPI_PINMUX_PAD3			PINMUX_PA19C_SERCOM1_PAD3
+#define AT25DFX_SPI						SERCOM1
+#define AT25DFX_CS						PIN_PA07
+#define AT25DFX_MEM_TYPE				AT25DFX_081A
+static uint8_t read_buffer[AT25DFX_BUFFER_SIZE];
+//static uint8_t write_buffer[AT25DFX_BUFFER_SIZE] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+struct spi_module at25dfx_spi;
+struct at25dfx_chip_module at25dfx_chip;
+
+uint32_t flash_addr = 0x10000;
+
+/** File download processing state. */
+static download_state down_state = NOT_READY;
+
+/** UART module for debug. */
+static struct usart_module cdc_uart_module;
+
+/** Instance of Timer module. */
+struct sw_timer_module swt_module_inst;
+
+/** Instance of HTTP client module. */
+struct http_client_module http_client_module_inst;
+
+/**
+ * \brief Initialize download state to not ready.
+ */
+static void init_state(void)
+{
+	down_state = NOT_READY;
+}
+
+/**
+ * \brief Clear state parameter at download processing state.
+ * \param[in] mask Check download_state.
+ */
+static void clear_state(download_state mask)
+{
+	down_state &= ~mask;
+}
+
+/**
+ * \brief Add state parameter at download processing state.
+ * \param[in] mask Check download_state.
+ */
+static void add_state(download_state mask)
+{
+	down_state |= mask;
+}
+
+/**
+ * \brief File download processing state check.
+ * \param[in] mask Check download_state.
+ * \return true if this state is set, false otherwise.
+ */
+
+static inline bool is_state_set(download_state mask)
+{
+	return ((down_state & mask) != 0);
+}
+
+/**
+ * \brief Start file download via HTTP connection.
+ */
+static void start_download(void)
+{
+	/*
+	if (!is_state_set(STORAGE_READY)) {
+		printf("start_download: Flash not initialized.\r\n");
+		return;
+	}
+	*/
+	if (!is_state_set(WIFI_CONNECTED)) {
+		printf("start_download: Wi-Fi is not connected.\r\n");
+		return;
+	}
+
+	if (is_state_set(GET_REQUESTED)) {
+		printf("start_download: request is sent already.\r\n");
+		return;
+	}
+
+	if (is_state_set(DOWNLOADING)) {
+		printf("start_download: running download already.\r\n");
+		return;
+	}
+
+	/* Send the HTTP request. */
+	printf("start_download: sending HTTP request...\r\n");
+	http_client_send_request(&http_client_module_inst, MAIN_HTTP_FILE_URL, HTTP_METHOD_GET, NULL, NULL);
+}
+
+
+/**
+ * \brief Callback of the HTTP client.
+ *
+ * \param[in]  module_inst     Module instance of HTTP client module.
+ * \param[in]  type            Type of event.
+ * \param[in]  data            Data structure of the event. \refer http_client_data
+ */
+static void http_client_callback(struct http_client_module *module_inst, int type, union http_client_data *data)
+{
+	switch (type) {
+	case HTTP_CLIENT_CALLBACK_SOCK_CONNECTED:
+		printf("http_client_callback: HTTP client socket connected.\r\n");
+		break;
+
+	case HTTP_CLIENT_CALLBACK_REQUESTED:
+		printf("http_client_callback: request completed.\r\n");
+		add_state(GET_REQUESTED);
+		break;
+
+	case HTTP_CLIENT_CALLBACK_RECV_RESPONSE:
+		printf("http_client_callback: received response %u data size %u\r\n",
+				(unsigned int)data->recv_response.response_code,
+				(unsigned int)data->recv_response.content_length);
+		if ((unsigned int)data->recv_response.response_code == 200) {
+		} 
+		else {
+			add_state(CANCELED);
+			return;
+		}
+		if (data->recv_response.content_length <= MAIN_BUFFER_MAX_SIZE) {
+			//***store_file_packet(data->recv_response.content, data->recv_response.content_length);
+			
+			at25dfx_chip_wake(&at25dfx_chip);
+			if (at25dfx_chip_check_presence(&at25dfx_chip) != STATUS_OK) {
+				// Handle missing or non-responsive device
+			}
+			at25dfx_chip_set_sector_protect(&at25dfx_chip, 0x10000, false);				// unprotect sector
+			at25dfx_chip_erase_block(&at25dfx_chip, 0x10000, AT25DFX_BLOCK_SIZE_4KB);	// erase block
+			at25dfx_chip_write_buffer(&at25dfx_chip, 0x10000, data->recv_response.content, data->recv_response.content_length);	// write buffer
+			at25dfx_chip_read_buffer(&at25dfx_chip, 0x10FF6, read_buffer, AT25DFX_BUFFER_SIZE);		// read same location only first ten bytes
+			at25dfx_chip_set_global_sector_protect(&at25dfx_chip, true);				// protect sector
+			at25dfx_chip_sleep(&at25dfx_chip);											// back to sleep
+			
+			add_state(COMPLETED);
+		}
+		break;
+
+	case HTTP_CLIENT_CALLBACK_RECV_CHUNKED_DATA:
+		printf("http_client_callback_CHUNKED DATA: received response data size %u\r\n",
+				(unsigned int)data->recv_chunked_data.length);
+		//***store_file_packet(data->recv_chunked_data.data, data->recv_chunked_data.length);
+		// NEED TO UPDATE THE ADDRESS LOCATION
+		at25dfx_chip_wake(&at25dfx_chip);
+		if (at25dfx_chip_check_presence(&at25dfx_chip) != STATUS_OK) {
+			// Handle missing or non-responsive device
+		}
+		at25dfx_chip_set_sector_protect(&at25dfx_chip, flash_addr, false);				// unprotect sector
+		at25dfx_chip_erase_block(&at25dfx_chip, flash_addr, AT25DFX_BLOCK_SIZE_4KB);	// erase block
+		at25dfx_chip_write_buffer(&at25dfx_chip, flash_addr, data->recv_chunked_data.data, data->recv_chunked_data.length);	// write buffer
+		at25dfx_chip_read_buffer(&at25dfx_chip, flash_addr, read_buffer, AT25DFX_BUFFER_SIZE);		// read same location
+		at25dfx_chip_read_buffer(&at25dfx_chip, (flash_addr+0x0020), read_buffer, AT25DFX_BUFFER_SIZE);		// read same location
+		at25dfx_chip_set_global_sector_protect(&at25dfx_chip, true);				// protect sector
+		at25dfx_chip_sleep(&at25dfx_chip);											// back to sleep
+		flash_addr = flash_addr + (uint32_t)data->recv_chunked_data.length;
+		
+		if (data->recv_chunked_data.is_complete) {
+			add_state(COMPLETED);
+		}
+
+		break;
+
+	case HTTP_CLIENT_CALLBACK_DISCONNECTED:
+		printf("http_client_callback: disconnection reason:%d\r\n", data->disconnected.reason);
+
+		/* If disconnect reason is equal to -ECONNRESET(-104),
+		 * It means the server has closed the connection (timeout).
+		 * This is normal operation.
+		 */
+		if (data->disconnected.reason == -EAGAIN) {
+			/* Server has not responded. Retry immediately. */
+			if (is_state_set(DOWNLOADING)) {
+				//f_close(&file_object);
+				clear_state(DOWNLOADING);
+			}
+
+			if (is_state_set(GET_REQUESTED)) {
+				clear_state(GET_REQUESTED);
+			}
+
+			start_download();
+		}
+
+		break;
+	}
+}
+
+/**
+ * \brief Callback to get the data from socket.
+ *
+ * \param[in] sock socket handler.
+ * \param[in] u8Msg socket event type. Possible values are:
+ *  - SOCKET_MSG_BIND
+ *  - SOCKET_MSG_LISTEN
+ *  - SOCKET_MSG_ACCEPT
+ *  - SOCKET_MSG_CONNECT
+ *  - SOCKET_MSG_RECV
+ *  - SOCKET_MSG_SEND
+ *  - SOCKET_MSG_SENDTO
+ *  - SOCKET_MSG_RECVFROM
+ * \param[in] pvMsg is a pointer to message structure. Existing types are:
+ *  - tstrSocketBindMsg
+ *  - tstrSocketListenMsg
+ *  - tstrSocketAcceptMsg
+ *  - tstrSocketConnectMsg
+ *  - tstrSocketRecvMsg
+ */
+static void socket_cb(SOCKET sock, uint8_t u8Msg, void *pvMsg)
+{
+	http_client_socket_event_handler(sock, u8Msg, pvMsg);
+}
+
+/**
+ * \brief Callback for the gethostbyname function (DNS Resolution callback).
+ * \param[in] pu8DomainName Domain name of the host.
+ * \param[in] u32ServerIP Server IPv4 address encoded in NW byte order format. If it is Zero, then the DNS resolution failed.
+ */
+static void resolve_cb(uint8_t *pu8DomainName, uint32_t u32ServerIP)
+{
+	printf("resolve_cb: %s IP address is %d.%d.%d.%d\r\n\r\n", pu8DomainName,
+			(int)IPV4_BYTE(u32ServerIP, 0), (int)IPV4_BYTE(u32ServerIP, 1),
+			(int)IPV4_BYTE(u32ServerIP, 2), (int)IPV4_BYTE(u32ServerIP, 3));
+	http_client_socket_resolve_handler(pu8DomainName, u32ServerIP);
+}
+
+/**
+ * \brief Callback to get the Wi-Fi status update.
+ *
+ * \param[in] u8MsgType type of Wi-Fi notification. Possible types are:
+ *  - [M2M_WIFI_RESP_CURRENT_RSSI](@ref M2M_WIFI_RESP_CURRENT_RSSI)
+ *  - [M2M_WIFI_RESP_CON_STATE_CHANGED](@ref M2M_WIFI_RESP_CON_STATE_CHANGED)
+ *  - [M2M_WIFI_RESP_CONNTION_STATE](@ref M2M_WIFI_RESP_CONNTION_STATE)
+ *  - [M2M_WIFI_RESP_SCAN_DONE](@ref M2M_WIFI_RESP_SCAN_DONE)
+ *  - [M2M_WIFI_RESP_SCAN_RESULT](@ref M2M_WIFI_RESP_SCAN_RESULT)
+ *  - [M2M_WIFI_REQ_WPS](@ref M2M_WIFI_REQ_WPS)
+ *  - [M2M_WIFI_RESP_IP_CONFIGURED](@ref M2M_WIFI_RESP_IP_CONFIGURED)
+ *  - [M2M_WIFI_RESP_IP_CONFLICT](@ref M2M_WIFI_RESP_IP_CONFLICT)
+ *  - [M2M_WIFI_RESP_P2P](@ref M2M_WIFI_RESP_P2P)
+ *  - [M2M_WIFI_RESP_AP](@ref M2M_WIFI_RESP_AP)
+ *  - [M2M_WIFI_RESP_CLIENT_INFO](@ref M2M_WIFI_RESP_CLIENT_INFO)
+ * \param[in] pvMsg A pointer to a buffer containing the notification parameters
+ * (if any). It should be casted to the correct data type corresponding to the
+ * notification type. Existing types are:
+ *  - tstrM2mWifiStateChanged
+ *  - tstrM2MWPSInfo
+ *  - tstrM2MP2pResp
+ *  - tstrM2MAPResp
+ *  - tstrM2mScanDone
+ *  - tstrM2mWifiscanResult
+ */
+static void wifi_cb(uint8_t u8MsgType, void *pvMsg)
+{
+	switch (u8MsgType) {
+	case M2M_WIFI_RESP_CON_STATE_CHANGED:
+	{
+		tstrM2mWifiStateChanged *pstrWifiState = (tstrM2mWifiStateChanged *)pvMsg;
+		if (pstrWifiState->u8CurrState == M2M_WIFI_CONNECTED) {
+			printf("wifi_cb: M2M_WIFI_CONNECTED\r\n");
+			m2m_wifi_request_dhcp_client();
+		} else if (pstrWifiState->u8CurrState == M2M_WIFI_DISCONNECTED) {
+			printf("wifi_cb: M2M_WIFI_DISCONNECTED\r\n");
+			clear_state(WIFI_CONNECTED);
+			if (is_state_set(DOWNLOADING)) {
+				clear_state(DOWNLOADING);
+			}
+
+			if (is_state_set(GET_REQUESTED)) {
+				clear_state(GET_REQUESTED);
+			}
+
+			m2m_wifi_connect((char *)MAIN_WLAN_SSID, sizeof(MAIN_WLAN_SSID),
+					MAIN_WLAN_AUTH, (char *)MAIN_WLAN_PSK, M2M_WIFI_CH_ALL);
+		}
+
+		break;
+	}
+
+	case M2M_WIFI_REQ_DHCP_CONF:
+	{
+		uint8_t *pu8IPAddress = (uint8_t *)pvMsg;
+		printf("wifi_cb: IP address is %u.%u.%u.%u\r\n",
+				pu8IPAddress[0], pu8IPAddress[1], pu8IPAddress[2], pu8IPAddress[3]);
+		add_state(WIFI_CONNECTED);
+		start_download();
+		break;
+	}
+
+	default:
+		break;
+	}
+}
+
+/**
+ * \brief Configure UART console.
+ */
+static void configure_console(void)
+{
+	struct usart_config usart_conf;
+
+	usart_get_config_defaults(&usart_conf);
+	usart_conf.mux_setting = EDBG_CDC_SERCOM_MUX_SETTING;
+	usart_conf.pinmux_pad0 = EDBG_CDC_SERCOM_PINMUX_PAD0;
+	usart_conf.pinmux_pad1 = EDBG_CDC_SERCOM_PINMUX_PAD1;
+	usart_conf.pinmux_pad2 = EDBG_CDC_SERCOM_PINMUX_PAD2;
+	usart_conf.pinmux_pad3 = EDBG_CDC_SERCOM_PINMUX_PAD3;
+	usart_conf.baudrate    = 115200;
+
+	stdio_serial_init(&cdc_uart_module, EDBG_CDC_MODULE, &usart_conf);
+	usart_enable(&cdc_uart_module);
+}
+
+/**
+ * \brief Configure Timer module.
+ */
+static void configure_timer(void)
+{
+	struct sw_timer_config swt_conf;
+	sw_timer_get_config_defaults(&swt_conf);
+
+	sw_timer_init(&swt_module_inst, &swt_conf);
+	sw_timer_enable(&swt_module_inst);
+}
+
+/**
+ * \brief Configure HTTP client module.
+ */
+static void configure_http_client(void)
+{
+	struct http_client_config httpc_conf;
+	int ret;
+
+	http_client_get_config_defaults(&httpc_conf);
+
+	httpc_conf.recv_buffer_size = MAIN_BUFFER_MAX_SIZE;
+	httpc_conf.timer_inst = &swt_module_inst;
+
+	ret = http_client_init(&http_client_module_inst, &httpc_conf);
+	if (ret < 0) {
+		printf("configure_http_client: HTTP client initialization failed! (res %d)\r\n", ret);
+		while (1) {
+		} /* Loop forever. */
+	}
+
+	http_client_register_callback(&http_client_module_inst, http_client_callback);
+}
+
+static void at25dfx_init(void)
+{
+	struct at25dfx_chip_config at25dfx_chip_config;
+	struct spi_config at25dfx_spi_config;
+	at25dfx_spi_get_config_defaults(&at25dfx_spi_config);
+	at25dfx_spi_config.mode_specific.master.baudrate = AT25DFX_CLOCK_SPEED;
+	at25dfx_spi_config.mux_setting = AT25DFX_SPI_PINMUX_SETTING;
+	at25dfx_spi_config.pinmux_pad0 = AT25DFX_SPI_PINMUX_PAD0;
+	at25dfx_spi_config.pinmux_pad1 = AT25DFX_SPI_PINMUX_PAD1;
+	at25dfx_spi_config.pinmux_pad2 = AT25DFX_SPI_PINMUX_PAD2;
+	at25dfx_spi_config.pinmux_pad3 = AT25DFX_SPI_PINMUX_PAD3;
+	spi_init(&at25dfx_spi, AT25DFX_SPI, &at25dfx_spi_config);
+	spi_enable(&at25dfx_spi);
+	
+	at25dfx_chip_config.type = AT25DFX_MEM_TYPE;
+	at25dfx_chip_config.cs_pin = AT25DFX_CS;
+	at25dfx_chip_init(&at25dfx_chip, &at25dfx_spi, &at25dfx_chip_config);
+}
+
+/**
+ * \brief Main application function.
+ *
+ * Application entry point.
+ *
+ * \return program return value.
+ */
+int main(void)
+{
+	tstrWifiInitParam param;
+	int8_t ret;
+	init_state();
+
+	/* Initialize the board. */
+	system_init();
+	
+	/* Initialize the UART console. */
+	configure_console();
+	printf(STRING_HEADER);
+	printf("\r\nThis application requires the AP to have internet access.\r\n\r\n");
+
+	/* Initialize the Timer. */
+	configure_timer();
+
+	/* Initialize the HTTP client service. */
+	configure_http_client();
+
+	/* Initialize the BSP. Part of WiFi*/
+	nm_bsp_init();
+
+	/* Initialize Flash*/
+	at25dfx_init();
+	
+	/* Initialize Wi-Fi parameters structure to zero */
+	memset((uint8_t *)&param, 0, sizeof(tstrWifiInitParam));
+
+	/* Initialize Wi-Fi driver with data and status callbacks. */
+	param.pfAppWifiCb = wifi_cb;
+	ret = m2m_wifi_init(&param);
+	if (M2M_SUCCESS != ret) {
+		printf("main: m2m_wifi_init call error! (res %d)\r\n", ret);
+		while (1) {
+		}
+	}
+
+	/* Initialize socket module. */
+	socketInit();
+	/* Register socket callback function. */
+	registerSocketCallback(socket_cb, resolve_cb);
+
+	/* Connect to router. */
+	printf("main: connecting to WiFi AP %s...\r\n", (char *)MAIN_WLAN_SSID);
+	m2m_wifi_connect((char *)MAIN_WLAN_SSID, sizeof(MAIN_WLAN_SSID), MAIN_WLAN_AUTH, (char *)MAIN_WLAN_PSK, M2M_WIFI_CH_ALL);
+
+	while (!(is_state_set(COMPLETED) || is_state_set(CANCELED))) {
+		/* Handle pending events from network controller. */
+		m2m_wifi_handle_events(NULL);
+		/* Checks the timer timeout. */
+		sw_timer_task(&swt_module_inst);
+	}
+	//printf("main: please unplug the SD/MMC card.\r\n");
+	printf("main: done.\r\n");
+
+	while (1) {
+		/* Is button pressed? */
+		if (port_pin_get_input_level(BUTTON_0_PIN) == BUTTON_0_ACTIVE) {
+			/* Yes, so turn LED on. */
+			port_pin_set_output_level(LED_0_PIN, LED_0_ACTIVE);
+			} else {
+			/* No, so turn LED off. */
+			port_pin_set_output_level(LED_0_PIN, !LED_0_ACTIVE);
+		}
+	}
+	
+	return 0;
+}
